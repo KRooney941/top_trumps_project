fb64849340e3059eb88f66cba164e8c9ac03364a
Kieran Rooney <kieranrooney@Kierans-MacBook-Pro.local>
2022-07-02 12:31:27 +0100

    all working but update in back end

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -1,28 +1,44 @@
 const express = require('express');
 const ObjectID = require('mongodb').ObjectID;
 
-const createRouter = function(collection) {
+const createRouter = function (collection) {
 
   const router = express.Router();
 
+
+  //index route
   router.get('/', (req, res) => {
     collection
-    .find()
-    .toArray()
-    .then((docs) => res.json(docs))
-    .catch((err) => {
-      console.error(err);
-      res.status(500);
-      res.json({ status: 500, error: err });
-    });
+      .find()
+      .toArray()
+      .then((docs) => res.json(docs))
+      .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({ status: 500, error: err });
+      });
+  });
+ 
+  //show route
+  router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    collection
+      .findOne({ _id: ObjectID(id) })
+      .then((doc) => res.json(doc))
+      .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({ status: 500, error: err });
+      });
   });
 
+  //Create route. collection is database information. puts the data from result(res is short for result) to the top of the list at the result.ops. 
   router.post('/', (req, res) => {
-    const newData = req.body;
+    const newData = req.body; 
     collection
     .insertOne(newData)
     .then((result) => {
-      res.json(result.ops[0]);
+      res.json(result.ops[0])
     })
     .catch((err) => {
       console.error(err);
@@ -31,37 +47,43 @@ const createRouter = function(collection) {
     });
   });
 
-  router.put('/:id', (req, res) => {
-    const id = req.params.id;
-    const updatedData = req.body;
-    delete updatedData._id;
 
-    collection
-    .updateOne({ _id: ObjectID(id) }, { $set: updatedData })
-    .then(result => {
-      res.json(result);
-    })
-    .catch((err) => {
-      res.status(500);
-      res.json({ status: 500, error: err });
-    });
-  });
-
+  //Destroy route. Use collection (this is basically our seeds) as that's what we want to change. Line 56 makes changes to json. 
   router.delete('/:id', (req, res) => {
     const id = req.params.id;
     collection
-    .deleteOne({ _id: ObjectID(id) })
-    .then(result => {
-      res.json(result);
-    })
+    .deleteOne({_id: ObjectID(id)})
+    .then((docs) => result.json(docs))
     .catch((err) => {
       console.error(err);
       res.status(500);
       res.json({ status: 500, error: err });
     });
-  });
+  })
+  
+  //Update route. 
+  router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const updatedData = req.body;
+    collection
+    .updateOne(
+      {_id: ObjectID(id)},
+      {$set: updatedData}
+    )
+    .then((result) => {
+      res.json(result)
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err });
+    });    
+  })
 
   return router;
 };
+
+
+
 
 module.exports = createRouter;
